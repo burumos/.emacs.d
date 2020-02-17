6886186b38f1b4e4ffe6925b851733c80f193feb
Naoki Kawajiri(burumos) <kanaoki19940317@mail.com>
2020-02-18 06:34:06 +0900

    helm find file command

--- elisp/packages-1.el
+++ elisp/packages-1.el
@@ -300,8 +300,9 @@
                    (add-to-list 'helm-completing-read-handlers-alist '(find-file . nil))
 
                    (define-key helm-map (kbd "C-h") 'delete-backward-char)
+                   (define-key helm-find-files-map (kbd "ESC") nil)
                    (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
-                   (define-key helm-find-files-map (kbd "C-[") 'helm-find-files-up-one-level)
+                   (define-key helm-find-files-map (kbd "M-C-h") 'helm-find-files-up-one-level)
                    ;;(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
                    ;;(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
